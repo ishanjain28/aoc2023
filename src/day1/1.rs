97bec285ced1e1cf9b5ef7cb0bc71fd6f9c1a800
Ishan Jain <contact@ishanjain.me>
2023-12-01 11:15:53 +0530

    added day 1-1

--- src/day1/1.rs
+++ src/day1/1.rs
@@ -1,1 +1,23 @@
 const INPUTS: [&'static str; 2] = [include_str!("./sample.txt"), include_str!("./input.txt")];
+
+fn main() {
+    for input in INPUTS.iter() {
+        let mut total = 0;
+        for line in input.split('\n') {
+            let first = line
+                .chars()
+                .find(|c| c.is_numeric())
+                .map_or(0, |x| 10 * x.to_digit(10).unwrap());
+
+            let last = line
+                .chars()
+                .rev()
+                .find(|c| c.is_numeric())
+                .map_or(0, |x| x.to_digit(10).unwrap());
+
+            total += (first + last);
+        }
+
+        println!("total = {}", total);
+    }
+}
