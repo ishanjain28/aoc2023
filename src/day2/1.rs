1e5f2305041763caad2ef6458a8c3e3780aa55e5
Ishan Jain <contact@ishanjain.me>
2023-12-02 11:03:06 +0530

    day2: added solutions

--- src/day2/1.rs
+++ src/day2/1.rs
@@ -1,1 +1,91 @@
-const INPUTS: [&'static str; 2] = [include_str!("./sample.txt"), include_str!("./input.txt")];
+#![feature(test)]
+extern crate test;
+
+const INPUTS: [&str; 2] = [include_str!("./sample.txt"), include_str!("./input.txt")];
+
+#[derive(Debug)]
+pub enum Move {
+    Red(u32),
+    Green(u32),
+    Blue(u32),
+}
+
+fn process(data: &str) -> u32 {
+    let mut total = 0;
+
+    for line in data.lines() {
+        if line.is_empty() {
+            continue;
+        }
+        let (gid, remain) = line.split_once(':').unwrap();
+
+        let gid = gid
+            .chars()
+            .filter(|c| c.is_numeric())
+            .collect::<String>()
+            .parse::<u32>()
+            .unwrap();
+
+        let moves = remain.split(';').map(|x| {
+            let mut output = vec![];
+            let cubes = x.split(',');
+
+            for cube in cubes {
+                let cube = cube.trim();
+                let (count, color) = cube.split_once(' ').unwrap();
+
+                let count = count.parse::<u32>().unwrap();
+
+                match color {
+                    "red" => output.push(Move::Red(count)),
+                    "green" => output.push(Move::Green(count)),
+                    "blue" => output.push(Move::Blue(count)),
+                    _ => unreachable!(),
+                }
+            }
+
+            output
+        });
+
+        let mut possible = true;
+        'outer: for mmove in moves {
+            for step in mmove {
+                match step {
+                    Move::Red(v) if v > 12 => {
+                        possible = false;
+                        break 'outer;
+                    }
+                    Move::Green(v) if v > 13 => {
+                        possible = false;
+                        break 'outer;
+                    }
+                    Move::Blue(v) if v > 14 => {
+                        possible = false;
+                        break 'outer;
+                    }
+
+                    _ => (),
+                }
+            }
+        }
+        if possible {
+            total += gid;
+        }
+    }
+
+    total
+}
+
+fn main() {
+    for input in INPUTS.iter() {
+        println!("total = {}", process(input));
+    }
+}
+
+#[bench]
+fn part1(b: &mut test::Bencher) {
+    b.iter(|| {
+        let v = process(INPUTS[1]);
+        test::black_box(v);
+    });
+}
--- src/day2/2.rs
+++ src/day2/2.rs
@@ -1,1 +1,73 @@
-const INPUTS: [&'static str; 2] = [include_str!("./sample.txt"), include_str!("./input.txt")];
+#![feature(test)]
+extern crate test;
+
+const INPUTS: [&str; 2] = [include_str!("./sample.txt"), include_str!("./input.txt")];
+
+#[derive(Debug)]
+pub enum Move {
+    Red(u32),
+    Green(u32),
+    Blue(u32),
+}
+
+fn process(data: &str) -> u32 {
+    let mut total = 0;
+
+    for line in data.lines() {
+        if line.is_empty() {
+            continue;
+        }
+        let (_, remain) = line.split_once(':').unwrap();
+
+        let moves = remain.split(';').flat_map(|x| {
+            let mut output = vec![];
+            let cubes = x.split(',');
+
+            for cube in cubes {
+                let cube = cube.trim();
+                let (count, color) = cube.split_once(' ').unwrap();
+
+                let count = count.parse::<u32>().unwrap();
+
+                match color {
+                    "red" => output.push(Move::Red(count)),
+                    "green" => output.push(Move::Green(count)),
+                    "blue" => output.push(Move::Blue(count)),
+                    _ => unreachable!(),
+                }
+            }
+
+            output
+        });
+
+        let mut min_red = 0;
+        let mut min_green = 0;
+        let mut min_blue = 0;
+
+        for mmove in moves {
+            match mmove {
+                Move::Red(v) => min_red = std::cmp::max(min_red, v),
+                Move::Green(v) => min_green = std::cmp::max(min_green, v),
+                Move::Blue(v) => min_blue = std::cmp::max(min_blue, v),
+            }
+        }
+
+        total += min_red * min_green * min_blue;
+    }
+
+    total
+}
+
+fn main() {
+    for input in INPUTS.iter() {
+        println!("total = {}", process(input));
+    }
+}
+
+#[bench]
+fn part1(b: &mut test::Bencher) {
+    b.iter(|| {
+        let v = process(INPUTS[1]);
+        test::black_box(v);
+    });
+}
