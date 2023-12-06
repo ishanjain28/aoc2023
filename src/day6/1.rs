9787958cc472e968c85528b4b7c5bcb042938eef
Ishan Jain <contact@ishanjain.me>
2023-12-06 10:46:11 +0530

    day6: added

--- src/day6/1.rs
+++ src/day6/1.rs
@@ -1,1 +1,64 @@
-const INPUTS: [&'static str; 2] = [include_str!("./sample.txt"), include_str!("./input.txt")];
+#![feature(test)]
+
+extern crate test;
+
+const INPUTS: [&str; 2] = [include_str!("./sample.txt"), include_str!("./input.txt")];
+
+fn process(data: &str) -> i64 {
+    let mut data = data.lines();
+
+    let time = data
+        .next()
+        .map(|line| {
+            line.split(' ')
+                .filter(|x| !x.is_empty())
+                .skip(1)
+                .map(|x| x.parse::<u64>().unwrap())
+        })
+        .unwrap();
+
+    let distance = data
+        .next()
+        .map(|line| {
+            line.split(' ')
+                .filter(|x| !x.is_empty())
+                .skip(1)
+                .map(|x| x.parse::<u64>().unwrap())
+        })
+        .unwrap();
+
+    let mut answer = 1;
+
+    for (time, dist) in time.zip(distance) {
+        let mut ways = 0;
+
+        for t in 0..time {
+            let time_left = time - t;
+            let speed = t;
+
+            let dist_traveled = time_left * speed;
+
+            if dist_traveled > dist {
+                ways += 1;
+            }
+        }
+
+        answer *= ways;
+    }
+
+    answer
+}
+
+fn main() {
+    for input in INPUTS.iter() {
+        println!("total = {}", process(input));
+    }
+}
+
+#[bench]
+fn part1(b: &mut test::Bencher) {
+    b.iter(|| {
+        let v = process(INPUTS[1]);
+        test::black_box(v);
+    });
+}
--- src/day6/2.rs
+++ src/day6/2.rs
@@ -1,1 +1,55 @@
-const INPUTS: [&'static str; 2] = [include_str!("./sample.txt"), include_str!("./input.txt")];
+#![feature(test)]
+
+extern crate test;
+
+const INPUTS: [&str; 2] = [include_str!("./sample.txt"), include_str!("./input.txt")];
+
+fn process(data: &str) -> i64 {
+    let mut data = data.lines();
+
+    let time: u64 = data
+        .next()
+        .map(|line| {
+            let x: String = line.chars().filter(|c| c.is_numeric()).collect();
+
+            x.parse::<u64>().unwrap()
+        })
+        .unwrap_or(0);
+    let distance: u64 = data
+        .next()
+        .map(|line| {
+            let x: String = line.chars().filter(|c| c.is_numeric()).collect();
+
+            x.parse::<u64>().unwrap()
+        })
+        .unwrap_or(0);
+
+    let mut answer = 0;
+
+    for t in 0..time {
+        let time_left = time - t;
+        let speed = t;
+
+        let dist_traveled = time_left * speed;
+
+        if dist_traveled > distance {
+            answer += 1;
+        }
+    }
+
+    answer
+}
+
+fn main() {
+    for input in INPUTS.iter() {
+        println!("total = {}", process(input));
+    }
+}
+
+#[bench]
+fn part1(b: &mut test::Bencher) {
+    b.iter(|| {
+        let v = process(INPUTS[1]);
+        test::black_box(v);
+    });
+}
