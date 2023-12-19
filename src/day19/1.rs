e156c2d7aaa1c3696ecbfcf1a93ec1a89c4c33e2
Ishan Jain <contact@ishanjain.me>
2023-12-19 13:13:23 +0530

    day19: added

--- src/day19/1.rs
+++ src/day19/1.rs
@@ -1,1 +1,165 @@
-const INPUTS: [&'static str; 2] = [include_str!("./sample.txt"), include_str!("./input.txt")];
+#![feature(slice_split_once)]
+#![feature(test)]
+
+use std::collections::{hash_map::Keys, HashMap};
+extern crate test;
+
+const INPUTS: [&str; 2] = [include_str!("./sample.txt"), include_str!("./input.txt")];
+
+#[derive(Debug)]
+struct Rule {
+    variable: Option<char>,
+    condition: Option<char>,
+    match_against: Option<i64>,
+    jump_to: String,
+}
+
+fn process(data: &str) -> i64 {
+    let mut answer = 0;
+
+    let mut data = data.split("\n\n");
+
+    let mut workflows = HashMap::new();
+
+    for workflow in data.next().unwrap().lines() {
+        let (name, remain) = workflow.split_once(|x| x == '{').unwrap();
+        let mut rules = vec![];
+        for branch in remain.split(',') {
+            let rule = if branch.contains(|x| x == '<' || x == '>') {
+                let branch: Vec<char> = branch.chars().collect();
+                let variable = branch[0];
+                let condition = branch[1];
+                let num: String = branch[2..]
+                    .iter()
+                    .take_while(|&x| x.is_ascii_digit())
+                    .collect();
+                let num: i64 = num.parse::<i64>().unwrap();
+
+                let mut jump_to = String::new();
+                for &c in branch.iter().rev() {
+                    if c == '}' {
+                        continue;
+                    }
+
+                    if c == ':' {
+                        break;
+                    }
+
+                    jump_to.push(c);
+                }
+
+                let jump_to: String = jump_to.chars().rev().collect();
+
+                Rule {
+                    variable: Some(variable),
+                    condition: Some(condition),
+                    match_against: Some(num),
+                    jump_to,
+                }
+            } else {
+                let branch: Vec<char> = branch.chars().collect();
+
+                let mut jump_to = String::new();
+                for &c in branch.iter().rev() {
+                    if c == '}' {
+                        continue;
+                    }
+
+                    if c == ':' {
+                        break;
+                    }
+
+                    jump_to.push(c);
+                }
+
+                let jump_to: String = jump_to.chars().rev().collect();
+
+                Rule {
+                    variable: None,
+                    condition: None,
+                    match_against: None,
+                    jump_to,
+                }
+            };
+            rules.push(rule);
+        }
+
+        workflows.insert(name, rules);
+    }
+
+    for input in data.next().unwrap().lines() {
+        let input: Vec<char> = input
+            .chars()
+            .filter(|&x| x.is_ascii_alphabetic() || x.is_ascii_digit() || x == ',')
+            .collect();
+
+        let mut vars = HashMap::new();
+
+        for set in input.split(|&x| x == ',') {
+            let variable = set[0];
+            let value: String = set[1..].iter().collect();
+            let value: i64 = value.parse::<i64>().unwrap();
+
+            vars.insert(variable, value);
+        }
+
+        let mut wname = "in";
+
+        let mut accepted = false;
+
+        loop {
+            if wname == "A" {
+                accepted = true;
+                break;
+            } else if wname == "R" {
+                accepted = false;
+                break;
+            }
+
+            let rules = workflows.get(&wname).unwrap();
+
+            for rule in rules {
+                if let Some(variable) = rule.variable {
+                    let var_value = *vars.get(&variable).unwrap();
+                    let check_against = rule.match_against.unwrap();
+
+                    let result = match rule.condition.unwrap() {
+                        '>' => var_value > check_against,
+                        '<' => var_value < check_against,
+                        _ => unreachable!(),
+                    };
+
+                    if result {
+                        wname = &rule.jump_to;
+                        break;
+                    }
+                } else {
+                    wname = &rule.jump_to;
+                    break;
+                }
+            }
+        }
+
+        if accepted {
+            for v in vars.values() {
+                answer += v;
+            }
+        }
+    }
+
+    answer
+}
+
+fn main() {
+    for input in INPUTS.iter() {
+        println!("answer = {}", process(input));
+    }
+}
+
+#[bench]
+fn part1(b: &mut test::Bencher) {
+    b.iter(|| {
+        let v = process(INPUTS[INPUTS.len() - 1]);
+        test::black_box(v);
+    });
+}
--- src/day19/2.rs
+++ src/day19/2.rs
@@ -1,1 +1,256 @@
-const INPUTS: [&'static str; 2] = [include_str!("./sample.txt"), include_str!("./input.txt")];
+#![feature(extract_if)]
+#![feature(slice_split_once)]
+#![feature(test)]
+
+use std::collections::HashMap;
+
+extern crate test;
+
+const INPUTS: [&str; 2] = [include_str!("./sample.txt"), include_str!("./input.txt")];
+
+#[derive(Debug)]
+struct Rule {
+    jump_to: String,
+    variable: char,
+    condition: char,
+    num: u64,
+}
+
+fn process(data: &str) -> u64 {
+    let mut data = data.split("\n\n");
+
+    let mut workflows = HashMap::new();
+
+    for workflow in data.next().unwrap().lines() {
+        let (name, remain) = workflow.split_once(|x| x == '{').unwrap();
+        let mut rules = vec![];
+        for branch in remain.split(',') {
+            let rule = if branch.contains(|x| x == '<' || x == '>') {
+                let branch: Vec<char> = branch.chars().collect();
+                let variable = branch[0];
+                let condition = branch[1];
+                let num: String = branch[2..]
+                    .iter()
+                    .take_while(|&x| x.is_ascii_digit())
+                    .collect();
+                let num: u64 = num.parse::<u64>().unwrap();
+
+                let mut jump_to = String::new();
+                for &c in branch.iter().rev() {
+                    if c == '}' {
+                        continue;
+                    }
+
+                    if c == ':' {
+                        break;
+                    }
+
+                    jump_to.push(c);
+                }
+
+                let jump_to: String = jump_to.chars().rev().collect();
+
+                Rule {
+                    jump_to,
+                    variable,
+                    condition,
+                    num,
+                }
+            } else {
+                let branch: Vec<char> = branch.chars().collect();
+
+                let mut jump_to = String::new();
+                for &c in branch.iter().rev() {
+                    if c == '}' {
+                        continue;
+                    }
+
+                    if c == ':' {
+                        break;
+                    }
+
+                    jump_to.push(c);
+                }
+
+                let jump_to: String = jump_to.chars().rev().collect();
+
+                Rule {
+                    jump_to,
+                    condition: ' ',
+                    num: 0,
+                    variable: ' ',
+                }
+            };
+            rules.push(rule);
+        }
+
+        workflows.insert(name, rules);
+    }
+
+    let mut answer = 0;
+
+    let mut set = Vec::new();
+    set.push(("in".to_string(), 1..=4000, 1..=4000, 1..=4000, 1..=4000));
+
+    while !set.is_empty() {
+        let mut next = Vec::new();
+
+        for set_ip in set.drain(..) {
+            let (ref wname, mut x, mut m, mut a, mut s) = set_ip.clone();
+            let rules = workflows.get(wname.as_str()).unwrap();
+
+            for rule in rules {
+                match (rule.variable, rule.condition) {
+                    ('x', '>') if x.contains(&rule.num) => {
+                        let new_x = rule.num + 1..=*x.end();
+
+                        let remain_x = *x.start()..=rule.num;
+                        x = remain_x;
+
+                        if !new_x.is_empty() {
+                            next.push((
+                                rule.jump_to.clone(),
+                                new_x,
+                                m.clone(),
+                                a.clone(),
+                                s.clone(),
+                            ));
+                        }
+                    }
+                    ('x', '<') if x.contains(&rule.num) => {
+                        let new_x = *x.start()..=rule.num - 1;
+                        x = rule.num..=*x.end();
+                        if !new_x.is_empty() {
+                            next.push((
+                                rule.jump_to.clone(),
+                                new_x,
+                                m.clone(),
+                                a.clone(),
+                                s.clone(),
+                            ));
+                        }
+                    }
+                    ('m', '>') if m.contains(&rule.num) => {
+                        let new_m = rule.num + 1..=*m.end();
+                        m = *m.start()..=rule.num;
+
+                        if !new_m.is_empty() {
+                            next.push((
+                                rule.jump_to.clone(),
+                                x.clone(),
+                                new_m,
+                                a.clone(),
+                                s.clone(),
+                            ));
+                        }
+                    }
+                    ('m', '<') if m.contains(&rule.num) => {
+                        let new_m = *m.start()..=rule.num - 1;
+                        m = rule.num..=*m.end();
+
+                        if !new_m.is_empty() {
+                            next.push((
+                                rule.jump_to.clone(),
+                                x.clone(),
+                                new_m,
+                                a.clone(),
+                                s.clone(),
+                            ));
+                        }
+                    }
+                    ('a', '>') if a.contains(&rule.num) => {
+                        let new_a = rule.num + 1..=*a.end();
+                        a = *a.start()..=rule.num;
+                        if !new_a.is_empty() {
+                            next.push((
+                                rule.jump_to.clone(),
+                                x.clone(),
+                                m.clone(),
+                                new_a,
+                                s.clone(),
+                            ));
+                        }
+                    }
+                    ('a', '<') if a.contains(&rule.num) => {
+                        let new_a = *a.start()..=rule.num - 1;
+                        a = rule.num..=*a.end();
+
+                        if !new_a.is_empty() {
+                            next.push((
+                                rule.jump_to.clone(),
+                                x.clone(),
+                                m.clone(),
+                                new_a,
+                                s.clone(),
+                            ));
+                        }
+                    }
+                    ('s', '<') if s.contains(&rule.num) => {
+                        let new_s = *s.start()..=rule.num - 1;
+                        s = rule.num..=*s.end();
+
+                        if !new_s.is_empty() {
+                            next.push((
+                                rule.jump_to.clone(),
+                                x.clone(),
+                                m.clone(),
+                                a.clone(),
+                                new_s,
+                            ));
+                        }
+                    }
+                    ('s', '>') if s.contains(&rule.num) => {
+                        let new_s = rule.num + 1..=*s.end();
+                        s = *s.start()..=rule.num;
+                        if !new_s.is_empty() {
+                            next.push((
+                                rule.jump_to.clone(),
+                                x.clone(),
+                                m.clone(),
+                                a.clone(),
+                                new_s,
+                            ));
+                        }
+                    }
+                    (' ', ' ') => {
+                        next.push((
+                            rule.jump_to.clone(),
+                            x.clone(),
+                            m.clone(),
+                            a.clone(),
+                            s.clone(),
+                        ));
+                    }
+                    _ => unreachable!(),
+                }
+            }
+        }
+
+        next.retain(|(wname, _, _, _, _)| wname != "R");
+
+        for (_, x, m, a, s) in next.extract_if(|(wname, _, _, _, _)| wname == "A") {
+            answer += (x.end() - x.start() + 1)
+                * (m.end() - m.start() + 1)
+                * (a.end() - a.start() + 1)
+                * (s.end() - s.start() + 1);
+        }
+
+        set = next;
+    }
+
+    answer
+}
+
+fn main() {
+    for input in INPUTS.iter() {
+        println!("answer = {}", process(input));
+    }
+}
+
+#[bench]
+fn part2(b: &mut test::Bencher) {
+    b.iter(|| {
+        let v = process(INPUTS[INPUTS.len() - 1]);
+        test::black_box(v);
+    });
+}
