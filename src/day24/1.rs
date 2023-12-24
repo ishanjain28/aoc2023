94e3e4b08e71cf6e9af8a8bee6bba6344a67da5e
Ishan Jain <contact@ishanjain.me>
2023-12-24 15:02:31 +0530

    day24: added

--- src/day24/1.rs
+++ src/day24/1.rs
@@ -1,1 +1,93 @@
-const INPUTS: [&'static str; 2] = [include_str!("./sample.txt"), include_str!("./input.txt")];
+#![feature(test)]
+
+use std::ops::RangeInclusive;
+
+extern crate test;
+
+const INPUTS: [&str; 1] = [include_str!("./input.txt")];
+
+fn process(data: &str) -> i64 {
+    let mut answer = 0;
+
+    let mut stones = vec![];
+    for line in data.lines() {
+        let (a, b) = line.split_once(" @ ").unwrap();
+
+        let mut pos = a
+            .split(',')
+            .map(|x| x.trim())
+            .map(|x| x.parse::<i64>().unwrap());
+
+        let mut vel = b
+            .split(',')
+            .map(|x| x.trim())
+            .map(|x| x.parse::<i64>().unwrap());
+
+        stones.push((
+            (pos.next().unwrap(), pos.next().unwrap()),
+            (vel.next().unwrap(), vel.next().unwrap()),
+        ));
+    }
+
+    for i in 0..stones.len() {
+        let a = stones[i];
+
+        let x1 = a.0 .0;
+        let y1 = a.0 .1;
+        let x2 = a.0 .0 + a.1 .0;
+        let y2 = a.0 .1 + a.1 .1;
+
+        let m1 = (y2 - y1) as f64 / (x2 - x1) as f64;
+        let c1 = y1 as f64 - m1 * x1 as f64;
+
+        for b in stones.iter().skip(i + 1) {
+            let x3 = b.0 .0;
+            let y3 = b.0 .1;
+            let x4 = b.0 .0 + b.1 .0;
+            let y4 = b.0 .1 + b.1 .1;
+
+            let m2 = (y4 - y3) as f64 / (x4 - x3) as f64;
+            let c2 = y3 as f64 - m2 * x3 as f64;
+
+            // parallel or coincidental
+            if m1 == m2 {
+                continue;
+            }
+
+            // y = m1 * x + c1;
+            // y = m2 * x + c2;
+            // x if y is equal
+            // (c1 - c2) / (m1 - m2)
+            // put this back in x and then y is
+            // y = m1 * i_x + c1
+            let i_x = (c2 - c1) / (m1 - m2);
+            let i_y = m1 * i_x + c1;
+
+            if (i_x - x1 as f64).signum() as i64 != a.1 .0.signum()
+                || (i_x - x3 as f64).signum() as i64 != b.1 .0.signum()
+            {
+                continue;
+            }
+
+            const LOOKUP_RANGE: RangeInclusive<f64> = 200000000000000.0..=400000000000000.0;
+
+            answer += (LOOKUP_RANGE.contains(&i_x) && LOOKUP_RANGE.contains(&i_y)) as i64
+        }
+    }
+
+    answer
+}
+
+fn main() {
+    for input in INPUTS.iter() {
+        println!("answer = {}", process(input));
+    }
+}
+
+#[bench]
+fn part1(b: &mut test::Bencher) {
+    b.iter(|| {
+        let v = process(INPUTS[INPUTS.len() - 1]);
+        test::black_box(v);
+    });
+}
--- src/day24/2.rs
+++ src/day24/2.rs
@@ -1,1 +1,84 @@
-const INPUTS: [&'static str; 2] = [include_str!("./sample.txt"), include_str!("./input.txt")];
+#![feature(test)]
+
+use z3::{
+    ast::{Ast, Int},
+    Config, Context, SatResult, Solver,
+};
+
+extern crate test;
+
+const INPUTS: [&str; 2] = [include_str!("./sample.txt"), include_str!("./input.txt")];
+
+fn process(data: &str) -> i64 {
+    let config = Config::new();
+    let ctx = Context::new(&config);
+    let solver = Solver::new(&ctx);
+
+    let x = Int::new_const(&ctx, "X");
+    let v_x = Int::new_const(&ctx, "V_X");
+    let y = Int::new_const(&ctx, "Y");
+    let v_y = Int::new_const(&ctx, "V_Y");
+    let z = Int::new_const(&ctx, "Z");
+    let v_z = Int::new_const(&ctx, "V_Z");
+    let zero = Int::from_i64(&ctx, 0);
+
+    let mut i = 0;
+    for line in data.lines() {
+        i += 1;
+        let (a, b) = line.split_once(" @ ").unwrap();
+
+        let pos: Vec<i64> = a
+            .split(',')
+            .map(|x| x.trim())
+            .map(|x| x.parse::<i64>().unwrap())
+            .collect();
+        let vel: Vec<i64> = b
+            .split(',')
+            .map(|x| x.trim())
+            .map(|x| x.parse::<i64>().unwrap())
+            .collect();
+
+        let s_x = Int::from_i64(&ctx, pos[0]);
+        let s_y = Int::from_i64(&ctx, pos[1]);
+        let s_z = Int::from_i64(&ctx, pos[2]);
+
+        let s_vx = Int::from_i64(&ctx, vel[0]);
+        let s_vy = Int::from_i64(&ctx, vel[1]);
+        let s_vz = Int::from_i64(&ctx, vel[2]);
+
+        let t = Int::new_const(&ctx, i);
+
+        // For every stone,
+        // X + V_X * t == sx + s_vx * t;
+        // Y + V_Y * t == sy + s_vy * t;
+        // Z + V_Z * t == sz + s_vz * t;
+
+        solver.assert(&t.ge(&zero));
+        solver.assert(&((&x + &v_x * &t)._eq(&(&s_x + &s_vx * &t))));
+        solver.assert(&((&y + &v_y * &t)._eq(&(&s_y + &s_vy * &t))));
+        solver.assert(&((&z + &v_z * &t)._eq(&(&s_z + &s_vz * &t))));
+    }
+
+    let result = solver.check();
+    if result != SatResult::Sat {
+        unreachable!();
+    }
+    let model = solver.get_model().unwrap();
+    let result = model.eval(&(&x + &y + &z), true).unwrap();
+
+    result.as_i64().unwrap()
+}
+
+fn main() {
+    for input in INPUTS.iter() {
+        println!("answer = {}", process(input));
+    }
+}
+
+#[bench]
+fn part1(b: &mut test::Bencher) {
+    b.iter(|| {
+        let v = process(INPUTS[INPUTS.len() - 1]);
+        test::black_box(v);
+    });
+}
